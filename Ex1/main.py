923471dd670374552320a18168e54a9df36742bb
Boris <Tsvetkovboris96@yandex.ru>
2023-05-21 05:00:21 +0300

    Update

--- Ex1/main.py
+++ Ex1/main.py
@@ -1,8 +1,8 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
-print("Введите трехзначное число: ")
+print("Введите трехзначное число: ", end=" ")
 number = int(input())
 first = number // 100
 second = number // 10 % 10
 third = number % 10
 summ = first + second + third
-print(f"Сумма цифр трехзначного числа {number} = ", summ)
+print(f"Сумма цифр трехзначного числа {number} = {summ}")
